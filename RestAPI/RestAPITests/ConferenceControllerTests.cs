9e350807a2c73fab27a8d37f6e6aab207db7a35e
Denis <den89181827071@mail.ru>
2020-04-04 13:42:48 +0300

    add some temlates for tests

---
+++ RestAPI/RestAPITests/ConferenceControllerTests.cs
@@ -0,0 +1,92 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using RestAPI.Controllers;
+using RestAPI.Models;
+using System.Collections.Generic;
+using System.IO;
+
+namespace RestAPITests
+{
+    public class ConferenceControllerTests
+    {
+        [Test]
+        public void Get()
+        {
+            var result = 13;
+            var testController = new ConferencesController();
+            Assert.AreEqual(result, testController.Get().Count);
+        }
+
+
+        [Test]
+        public void GetByTitleWithCorrectTitle()
+        {
+            var result = 13;
+            var testController = new ConferencesController();
+            Assert.AreEqual(result, testController.Get().Count);
+        }
+
+        [Test]
+        public void GetByTitleWithWrongTitle()
+        {
+
+        }
+
+        [Test]
+        public void GetByTitleWithIncorrectData()
+        {
+
+        }
+
+
+        [Test]
+        public void PostWithCorrectData()
+        {
+
+        }
+
+        [Test]
+        public void PostWithIncorrectData()
+        {
+
+        }
+
+
+        [Test]
+        public void PutWithCorrectData()
+        {
+
+        }
+
+        [Test]
+        public void PutWithCorrectIdAndIncorrectBody()
+        {
+
+        }
+
+        [Test]
+        public void PutWithIncorrectIdAndCorrectBody()
+        {
+
+        }
+
+        [Test]
+        public void PutWithIncorrectData()
+        {
+
+        }
+
+
+        [Test]
+        public void DeleteWithCorrectId()
+        {
+
+        }
+
+        [Test]
+        public void DeleteWithIncorrectId()
+        {
+
+        }
+    }
+}

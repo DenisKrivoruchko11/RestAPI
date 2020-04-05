270e3f527cfc4a1434f9e7e2c5951f4739b91c83
Denis <den89181827071@mail.ru>
2020-04-05 19:30:13 +0300

    add tests

--- RestAPI/RestAPITests/ConferenceControllerTests.cs
+++ RestAPI/RestAPITests/ConferenceControllerTests.cs
@@ -5,6 +5,7 @@ using RestAPI.Models;
 using System.Collections.Generic;
 using RestAPI.Common.CustomExceptions;
 using System;
+using NUnit.Framework.Internal;
 
 namespace RestAPITests
 {
@@ -13,9 +14,8 @@ namespace RestAPITests
         [Test]
         public void Get()
         {
-            var result = new List<ConferenceWithoutIDModel>();
             var testController = new ConferencesController();
-            Assert.AreEqual(result.GetType(), testController.Get().GetType());
+            Assert.DoesNotThrow(delegate { testController.Get(); });
         }
 
 
@@ -24,48 +24,62 @@ namespace RestAPITests
         {
             var result = new List<ConferenceWithoutIDModel>();
             var testController = new ConferencesController();
-            Assert.AreEqual(result.GetType(), testController.GetByTitle("Techfest 2019").GetType());
+            Assert.AreEqual(result.GetType(), testController.GetByTitle("SunshinePHP").GetType());
         }
 
         [Test]
         public void GetByTitleWithWrongTitle()
         {
-            var result = new NotFoudException("Conferences not found", 404);
             var testController = new ConferencesController();
-            //Assert.Throws(result, testController.GetByTitle("WrongName"));
+            Assert.Throws<NotFoudException>(delegate { testController.GetByTitle("WrongName"); });
         }
 
 
         [Test]
         public void Post()
         {
-
+            var testConference = new ConferenceWithoutIDModel();
+            var result = new Guid().ToString("N");
+            var testController = new ConferencesController();
+            Assert.AreEqual(result.GetType(), testController.Post(testConference).GetType());
         }
 
 
         [Test]
         public void PutWithCorrectData()
         {
-
+            var testConference = new ConferenceWithoutIDModel();
+            var testId = "5c092621a19ac14bd086c3ae";
+            var testController = new ConferencesController();
+            Assert.DoesNotThrow(delegate { testController.Put(testConference, testId); });
         }
 
         [Test]
         public void PutWithIncorrectId()
         {
-
+            var testConference = new ConferenceWithoutIDModel();
+            var testId = new Guid().ToString("N");
+            var testController = new ConferencesController();
+            Assert.Throws<NotFoudException>(delegate { testController.Put(testConference, testId); });
         }
 
 
-        [Test]
+      /*  [Test]
         public void DeleteWithCorrectId()
         {
-
-        }
+            var testConference = new ConferenceWithoutIDModel();
+            var testId = "5c542880c32bcb68a567680f";
+            var testController = new ConferencesController();
+            Assert.DoesNotThrow(delegate { testController.Delete(testId); });
+        } */
 
         [Test]
         public void DeleteWithIncorrectId()
         {
-
+            var testConference = new ConferenceWithoutIDModel();
+            var testId = new Guid().ToString("N");
+            var testController = new ConferencesController();
+            Assert.Throws<NotFoudException>(delegate { testController.Delete(testId); });
         }
     }
 }
